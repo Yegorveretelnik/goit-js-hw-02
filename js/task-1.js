17f4466210fa9c76b03672a01dbcbf0fcc0135a7
yegor <yegorveretelnik@gmail.com>
2024-12-09 20:46:42 +0100

    commit message

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,19 @@
-function makeTransaction(quantity, pricePerDroid) {
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
      let finalPrice = pricePerDroid*quantity
-     return "You ordered " + quantity + " droids worth " + finalPrice + " credits!"
+
+     if (customerCredits < finalPrice) {
+          return "Insufficient funds!"
+     }
+
+     else {
+          return "You ordered " + quantity + " droids worth " + finalPrice + " credits!"
+     }
+
 }
 
-console.log(makeTransaction(5, 3000)); 
-console.log(makeTransaction(3, 1000)); 
-console.log(makeTransaction(10, 500)); 
+console.log(makeTransaction(5, 3000, 23000)); 
+console.log(makeTransaction(3, 1000, 15000)); 
+console.log(makeTransaction(10, 5000, 8000)); 
+console.log(makeTransaction(8, 2000, 10000)); 
+console.log(makeTransaction(10, 500, 5000)); 
+
